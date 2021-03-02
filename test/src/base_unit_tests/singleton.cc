6d7c12a45cad3df49c2699d0ed09142c5b1ff3ad
Craciunas Andrei-Dragos (uic17137) <andrei-dragos.craciunas@continental-corporation.com>
2021-03-02 14:08:42 +0200

    Change test and base configuration

---
+++ test/src/base_unit_tests/singleton.cc
@@ -0,0 +1,31 @@
+#include <catch2/catch.hpp>
+#include <singleton.hpp>
+
+struct Object {
+    Object(std::string&& ss, int ii) :
+        s{ std::move(ss) },
+        i{ ii }
+    {}
+
+    Object(std::string&& ss) :
+        s{ std::move(ss) }
+    {}
+
+    std::string s{ "" };
+    int i{ 0 };
+};
+
+TEST_CASE(
+    "GIVEN" 
+    "multiple objects of the same class as template arguments for the singleton template,"
+    "THEN"
+    "calling different constructors will result in different objects while calling the same constructor"
+    "will fetch the originally created object") {
+
+    auto& instance1 = pat::Singleton<Object, std::string, int>::GetInstance("some", 30);
+    auto& instance2 = pat::Singleton<Object, std::string, int>::GetInstance("some", 30);
+    auto& instance3 = pat::Singleton<Object, std::string>::GetInstance("some");
+
+    REQUIRE(&instance1 == &instance2);
+    REQUIRE(&instance1 != &instance3);
+}
